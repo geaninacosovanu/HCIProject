95c234b223e3d0fb403ca01e31deb80768c2b50d
Bogdan Demian <ds.bogdan7@gmail.com>
2018-11-25 22:35:50 +0200

    added semaphore

--- js/presentation.js
+++ js/presentation.js
@@ -10,6 +10,28 @@ function load() {
                 setTimeout(()=>{
                     $('#bg').addClass('translate-car');
                     $('<embed src="sounds/autobuzul.mp3" width="180" height="90" loop="false" autostart="false" hidden="true">').appendTo('body');
+                    setTimeout(()=> {
+                        $("#passing").css('display', 'inherit');
+                        $("#cross_road").css('display', 'inherit');
+                    }, 10);
+                    setTimeout(()=>{
+                        $("#bg").removeClass('translate-car');
+                        $("#passing").css('animation', 'none');
+                        $("#cross_road").css('animation', 'none');
+
+                        $("#passing").addClass('stop-semaphore');
+                        $("#cross_road").addClass('stop-cross-road');
+                        setTimeout(()=>{
+                            $("#passing").attr('src', 'img/traffic-light-yellow.png');
+                            setTimeout(()=> {
+                                $("#passing").attr('src', 'img/traffic-light-green.png');
+                                $("#bg").addClass('translate-car');
+                                $("#passing").css('animation', 'continue-semaphore 21s forwards');
+                                $("#cross_road").css('animation', 'continue-cross-road 21s forwards');
+
+                            }, 3000);
+                        }, 3000)
+                    }, 4200);
                 },2000);
 
             }, 3000);
