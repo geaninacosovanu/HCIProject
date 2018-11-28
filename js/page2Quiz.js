d626beaf4e248f2d8b269d7d2d2241dd1c325614
Geanina Cosovanu <cosovanugeanina@gmail.com>
2018-11-28 18:53:01 +0200

    diff quiz done

--- js/page2Quiz.js
+++ js/page2Quiz.js
@@ -1,7 +1,37 @@
-function back(){
+function back() {
     window.location.replace('./page1Quiz.html')
 }
 
-function next(){
+function next() {
     window.location.replace('./page3Quiz.html')
+}
+
+let foundIds = [];
+let ids = [1, 2, 3, 4];
+let left = 0;
+
+function imageClick(imgNr) {
+    event.stopPropagation();
+
+    foundIds.push(imgNr);
+    const index = ids.indexOf(imgNr);
+    ids.splice(index, 1);
+    $('#' + imgNr).css('display', 'none');
+    $("#stars").append($('<img class="star animated zoomInDown " src="./img/star.png">').css('left', left + '%'));
+    left += 7;
+    if (foundIds.length === 4) {
+        $('#stars').append('<audio controls autoplay hidden src="sounds/happyKids.mp3" type="audio/mpeg"></audio>')
+        setTimeout(() => {
+            $('#back').css('display', 'block');
+            $('#next').css('display', 'block');
+        }, 4000);
+    }
+}
+
+
+function wrongClick() {
+    event.stopPropagation();
+    console.log('wrong click');
+    $('#' + ids[0]).addClass('animated pulse infinite');
+
 }
