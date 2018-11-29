01382441a1beb19cd5149da52d0522803acec4d6
Bogdan Demian <ds.bogdan7@gmail.com>
2018-11-29 19:24:42 +0200

    added stars

--- js/page4Quiz.js
+++ js/page4Quiz.js
@@ -1,13 +1,20 @@
+let left = 0;
 function startLightQuiz() {
 
 
     $("#red").click(() => {
         $("#red").css("display", "none");
         $("#traffic").attr("src", "img/traffic-light-red-glow.png");
+        appendStar();
         $("#yellow").click(() => {
+            appendStar();
             $("#yellow").css("display", "none");
             $("#traffic").attr("src", "img/traffic-light-yellow-glow.png");
             $("#green").click(() => {
+                appendStar();
+                setTimeout(()=> {
+                    appendStar();
+                }, 1000);
                 $("#blue").css("display", "none");
                 $("#green").css("display", "none");
                 $("#traffic").attr("src", "img/traffic-light-red.png");
@@ -21,4 +28,9 @@ function startLightQuiz() {
         });
     });
 
+}
+
+function appendStar() {
+    $("#stars").append($('<img class="star animated zoomInDown " src="./img/star.png">').css('left', left + '%'));
+    left += 7;
 }
