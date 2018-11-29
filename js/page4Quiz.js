67e909fa4038f46a85c89a7aaf2aad00414aaeb7
Bogdan Demian <ds.bogdan7@gmail.com>
2018-11-29 18:54:48 +0200

    lights

--- js/page4Quiz.js
+++ js/page4Quiz.js
@@ -1,6 +1,24 @@
 function startLightQuiz() {
-    $("#yellow").click(() => {
-        $("#yellow").css("display", "none");
-        $("#traffic").attr("src", );
-    })
+
+
+    $("#red").click(() => {
+        $("#red").css("display", "none");
+        $("#traffic").attr("src", "img/traffic-light-red-glow.png");
+        $("#yellow").click(() => {
+            $("#yellow").css("display", "none");
+            $("#traffic").attr("src", "img/traffic-light-yellow-glow.png");
+            $("#green").click(() => {
+                $("#blue").css("display", "none");
+                $("#green").css("display", "none");
+                $("#traffic").attr("src", "img/traffic-light-red.png");
+                setTimeout(()=> {
+                    $("#traffic").attr("src", "img/traffic-light-yellow.png");
+                    setTimeout(()=> {
+                        $("#traffic").attr("src", "img/traffic-light-green.png");
+                        }, 3000);
+                    }, 3000);
+            });
+        });
+    });
+
 }
